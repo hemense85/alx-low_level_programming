bf3dd2ae7d2674693f13d6862dfff8cbfc42c9ee
hemense85 <hemenatom@gmail.com>
2022-12-13 08:19:23 -0800

    task 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the alpha in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char lc;
+	for (lc = 'a'; lc <= 'z'; lc ++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}	
+
+     
