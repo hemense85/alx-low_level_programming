fcad7e21db083a82df2a3a44859d94738f8d277c
hemense85 <hemenatom@gmail.com>
2022-12-14 06:55:36 -0800

    task 2c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - print alpha in lower case
  *
  * Return: 0
  */
 int main(void)
+
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
 	int i;
-	for (i = 0; i < 26; i++);
+
+        for (i = 0; i < 26; i++);
 	{
 		putchar(aph[i]);
 	}
