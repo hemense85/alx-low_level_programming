36e8152af02e3aae25b0468b003add49bfe81524
hemense85 <hemenatom@gmail.com>
2022-12-14 07:58:13 -0800

    task 4c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int la;
 
-	for (la = 'a'; la <= 'z' la++)
+	for (la = 'a'; la <= 'z'; la++)
 	{
 		if (la != 'e' && la != 'q')
 			putchar(la);
 	}
-	putchar('\n')
-	return (0)
+	putchar('\n');
+	return (0);
 }
