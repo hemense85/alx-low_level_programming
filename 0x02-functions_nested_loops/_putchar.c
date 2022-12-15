58508f3ccd210b27169f9b46737eeeb4fcf22897
hemense85 <hemenatom@gmail.com>
2022-12-15 07:28:26 -0800

    . _putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
  * main - print putchar
- * written by mbah
- * Return; 0
+ *
+ * Return: 0
  */
-
 int main(void)
 {
 _putchar('_');
 _putchar('p');
-_putchar('w');
--putchar('t');
+_putchar('u');
+_putchar('t');
 _putchar('c');
--putchar('h');
+_putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n');
-return(0);
+-putchar('\n');
+return (0);
 }	
