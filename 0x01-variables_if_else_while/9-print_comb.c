775c741bbb17a1568ed80bd35881e083ae1a1a18
hemense85 <hemenatom@gmail.com>
2022-12-15 01:48:49 -0800

    task 9c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
-	
+	for (n = '0'; n < '9'; n++)
+	{
 		putchar(n);
-		if (n != 57)
+		if (n != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
