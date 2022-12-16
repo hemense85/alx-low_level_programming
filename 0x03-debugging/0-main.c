2206f86a915fb5b48074692b7ef47b5ce0b081fa
hemense85 <hemenatom@gmail.com>
2022-12-16 08:58:19 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test functions that print integer is postive or negative
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+postive _or_negative(i);
+
+return (0);
+
+}
