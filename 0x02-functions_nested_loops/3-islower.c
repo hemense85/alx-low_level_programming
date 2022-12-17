1ba4f407c68b495769e23aa9dd0d888b11fc6f8b
hemense85 <hemenatom@gmail.com>
2022-12-17 06:41:06 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,12 @@
 /**
  * _islower-ckeck for lower case charater
  * @c: the charater to be check
- * Return:0
+ * Return: 1 if the letter is lowcase, 0 if not
  */
 int _islower(int c)
 {
-	if (c >= 98 && c <= 122)
-	{
-	return (0);
-	}
-	return (0);
-}	
+	if (c >= 'a'  && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
